1a522360dee6cb9fd8b039ae305c6456a6e4856f
Sathu2005 <shathurjan.2005@gmail.com>
2025-06-21 10:09:25 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Class1
+    {
+        using System.Data.SQLite;
+
+public class Test
+    {
+        public void Connect()
+        {
+            var conn = new SQLiteConnection("Data Source=unicomtic.db");
+            conn.Open();
+        }
+    }
+
+}
+}
